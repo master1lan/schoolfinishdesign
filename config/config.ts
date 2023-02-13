9e50416acc32f321bd127e3b2a70a3c69c263daf
master1lan <278457198@qq.com>
2023-02-13 12:35:16 +0800

    update config

--- config/config.ts
+++ config/config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "umi";
 export default defineConfig({
   title: "信用案例分析系统",
   routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
+    { path: "/", component: "@/pages/index" },
+    // { path: "/docs", component: "docs" },
   ],
   npmClient: "pnpm",
   publicPath: "/schoolfinishdesign/",
