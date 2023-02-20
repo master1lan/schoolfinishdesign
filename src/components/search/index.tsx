1e092b7795578f9f03bc03f252a175d84e6695f3
master1lan <278457198@qq.com>
2023-02-20 16:03:25 +0800

    修改layout

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,5 +1,5 @@
 import { Button, Input } from "antd";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AdvanceSearch from "./advanceSearch";
 import style from "./search.module.less";
 const { Search } = Input;
@@ -7,6 +7,11 @@ const { Search } = Input;
 const Search_header = () => {
   const [show, set] = useState(false),
     handler = () => set((item) => !item);
+  useEffect(() => {
+    if (show) {
+      // window.addEventListener('click',)
+    }
+  }, []);
   return (
     <div className={style.content}>
       <div className={style.search}>
--- src/layouts/loginandregister.tsx
+++ src/layouts/loginandregister.tsx
@@ -1,8 +1,19 @@
-import { Card } from "antd";
-import { Outlet } from "umi";
+import { Button, Card, Divider, Form, Input } from "antd";
+import { Link, Outlet, useLocation } from "umi";
 import styles from "./login.module.less";
 import schoolLogo from "@/assets/school_logo.png";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+
+const useSelectORButton = () => {
+  const location = useLocation(),
+    isLoginPath = location.pathname === "/login",
+    ButtonName = isLoginPath ? "注册" : "登录",
+    linkTo = isLoginPath ? "/register" : "/login";
+  return { ButtonName, linkTo };
+};
+
 export default function LoginLayout() {
+  const { ButtonName, linkTo } = useSelectORButton();
   return (
     <Card className={styles.login_content}>
       <div className={styles.header}>
@@ -15,7 +26,44 @@ export default function LoginLayout() {
         />
         <p>信用案例分析系统</p>
       </div>
-      <Outlet />
+
+      <Form
+        name='normal_login'
+        className={styles["login-form"]}
+        initialValues={{ remember: true }}
+        //   onFinish={onFinish}
+      >
+        <Form.Item
+          name='username'
+          rules={[{ required: true, message: "请输入用户名" }]}
+        >
+          <Input
+            prefix={<UserOutlined className='site-form-item-icon' />}
+            placeholder='用户名'
+          />
+        </Form.Item>
+        <Form.Item
+          name='password'
+          rules={[{ required: true, message: "请输入密码" }]}
+        >
+          <Input
+            prefix={<LockOutlined className='site-form-item-icon' />}
+            type='password'
+            placeholder='密码'
+          />
+        </Form.Item>
+        <Form.Item>
+          <Outlet />
+        </Form.Item>
+        <Divider>或者</Divider>
+        <Form.Item>
+          <Link to={linkTo}>
+            <Button type='primary' htmlType='submit' block>
+              {ButtonName}
+            </Button>
+          </Link>
+        </Form.Item>
+      </Form>
     </Card>
   );
 }
